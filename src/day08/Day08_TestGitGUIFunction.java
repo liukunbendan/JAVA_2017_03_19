d5084f6cf681f372dbb20823a63a9e57b53525d8
Kun Liu <407958853@qq.com>
2017-04-20 18:37:41 +0800

    HP notebook- Tom update Java test file

---
+++ src/day08/Day08_TestGitGUIFunction.java
@@ -0,0 +1,12 @@
+package day08;
+
+public class Day08_TestGitGUIFunction {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	System.out.println("这个是用来测试JAVA功能正确与否的代码");
+	
+	}
+
+}
